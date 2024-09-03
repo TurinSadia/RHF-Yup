2a7fc0857ab4b180a54a0ed6ecafb26ab951c056
Kazi-Sadia <turin.sadia1001@gmail.com>
2024-09-04 01:01:34 +0600

    RHF+Yup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import YouTubeForm from "./components/form";
+import YupYouTubeForm from "./components/yupForm";
+
+function App() {
+  return (
+    <>
+      <YouTubeForm />
+      <YupYouTubeForm />
+    </>
+  );
+}
+
+export default App;
